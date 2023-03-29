2d04665fec062159c42fcee4ac90cab7395469fd
Temirlan Emls <temirlan.emls@gmail.com>
2023-03-29 14:48:11 +0600

    Fixed margin on HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,12 +5,12 @@ import { useGetProductsByLimitQuery } from '../store/fakeStoreApi/fakeStore.api'
 import ProductItem from './../components/ProductItem';
 import ErrorComp from '../components/ErrorComp';
 import LoadingComp from '../components/LoadingComp';
-import { useSetPageTitle } from "./../hooks/useSetPageTitle";
+import { useSetPageTitle } from './../hooks/useSetPageTitle';
 
 export default function HomePage() {
   const { data, isLoading, isError } = useGetProductsByLimitQuery(4);
 
-  useSetPageTitle('Home Page')
+  useSetPageTitle('Home Page');
 
   return (
     <section className='w-full mt-10 min-h-screen'>
@@ -23,7 +23,7 @@ export default function HomePage() {
         </div>
       </div>
       <Search />
-      <div className='mt-10 h-96 flex-col w-full flex justify-center items-center'>
+      <div className='mt-10 mb-20 h-96 flex-col w-full flex justify-center items-center'>
         {isError && <ErrorComp />}
         {isLoading && <LoadingComp />}{' '}
         <div className='w-full h-full grid grid-cols-4 gap-4'>
